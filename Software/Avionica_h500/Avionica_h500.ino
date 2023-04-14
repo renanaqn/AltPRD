0c526ef55957e219778f6628fd35daa08fea71b9
Renan de Aquino <renanap99@gmail.com>
2023-04-14 16:03:52 -0300

    Add files via upload

--- Software/Avionica_h500/Avionica_h500.ino
+++ Software/Avionica_h500/Avionica_h500.ino
@@ -6,7 +6,7 @@
 #include "BluetoothSerial.h" 
 
 #define flashCS 5 
-#define LED 2
+#define pressaoNivelMar = 1012
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED) 
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it 
@@ -43,7 +43,7 @@ void setup() {
   delay(500); 
 
   while (!bmp.begin(0x76)) { 
-    Serial.println("Erro no BMP"); 
+    Serial.println("Erro no BMP");
     delay(100); 
   }
   if (flash.begin(MB(8))) {
@@ -57,7 +57,7 @@ void setup() {
   Serial.print((long)(flash.getCapacity() / 1000));
   Serial.println(F("Kb"));
 
-  SerialBT.begin("ESP32test"); //Bluetooth device name
+  //SerialBT.begin("ESP32test"); //Bluetooth device name
   /*while(!SerialBT.hasClient()){ //CÓDIGO DO BT
   }*/
   
@@ -84,15 +84,17 @@ void writeString(String stringData){
 }
 
 void coleta() {
-  //CRIAR MILLIS E SALVAR NO WRITEFLOAT
   // Uma estimativa de 1045000 dados podem ser lidos durante aproximadamente 25min
   if (floatAddr > 4180000)ligada = false;
   if (ligada == true) {
-    floatAddr = flash.getAddress(sizeof(float));
-    float alt = bmp.readAltitude(1014.2);
-    flash.writeFloat(floatAddr, alt);
-    Serial.print("Float Address ");
-    Serial.println(floatAddr / 4);
+    floatAddr = flash.getAddress(sizeof(float)); 
+    delay(40);
+    flash.writeFloat(floatAddr, currentTime); 
+    Serial.print("Tempo: "); 
+    floatAddr = flash.getAddress(sizeof(float)); 
+    flash.writeFloat(floatAddr, alt); 
+    Serial.print("Float Address "); 
+    Serial.println(floatAddr / 4);     
     Serial.println("Valor: " + String(flash.readFloat(floatAddr)));
   }
 }
@@ -105,13 +107,16 @@ void reset_memory() {
 }
 
 void ler_dados () {
-  if (i > 1045000)ler = false;
+  if (i > 4180000)ler = false; 
   if (ler == true) {
     float valor = flash.readFloat(i);
-    Serial.println("End: ");
-    Serial.println(i);
-    Serial.println("Valor: ");
+    Serial.print("End: ");
+    Serial.println(i/4);
+    //Serial.print("Tempo: ");
+    //Serial.println(flash.readFloat(i));
+    Serial.print("Valor: ");
     Serial.println(valor);
+    
     delay(10);
     i = i + 4;
   }
@@ -120,10 +125,10 @@ void ler_dados () {
 void ejecao (){
   if (media2 < media1) {
     //comandar servo 5 vezes com delay de 50 ms
-} else {
-  valores1[i] = bmp.readAltitude(1014.2); 
-  valores2[i] = bmp.readAltitude(1014.2); 
-  media2 += valores1
+  } else {
+    valores1[i] = bmp.readAltitude(1014.2); 
+    valores2[i] = bmp.readAltitude(1014.2); 
+    media2 += valores1
   } 
 } */
 void loop() {
--- Software/Avionica_h500/core0.ino
+++ Software/Avionica_h500/core0.ino
@@ -1,9 +1,5 @@
 void loopCore0(void *arg ){
   while(true){
-      digitalWrite(LED,1);
-      delay(300);
-      digitalWrite(LED,0);
-      delay(300);
       coleta(); 
       ler_dados(); 
       //ejecao (); 
